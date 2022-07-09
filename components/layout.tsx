b9a2f6e6d974931b889355110a167da20bdf4934
Fabi <92405976+MotionbyFabi@users.noreply.github.com>
2022-07-09 17:42:13 +0200

    content origin

--- components/layout.tsx
+++ components/layout.tsx
@@ -14,6 +14,7 @@ const Layout: React.FC<Props> = ({ children, title }) => {
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        <meta name="referrer" content="origin" />
         <link rel="shortcut icon" type="image/jpeg" href="/favicon.jpg" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
