f17602b71aad2aa0f032a6c13958a55d0e73029f
motionbyfabi <fabiodinota@gmail.com>
2023-01-19 17:18:47 +0100

    fixes

--- components/HomePage/brands.tsx
+++ components/HomePage/brands.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
 import React from "react";
 import Image1 from "../../public/images/brands/cameo.png";
-import Image2 from "../../public/images/brands/gameshow.png";
+import Image9 from "../../public/images/brands/gameshow.png";
 import Image3 from "../../public/images/brands/halara.png"; //
 import Image16 from "../../public/images/brands/holoash.png"; // 
 import Image15 from "../../public/images/brands/infinix.png"; // 
 import Image11 from "../../public/images/brands/lifeboard.png"; //
 import Image8 from "../../public/images/brands/logitech.png";
-import Image7 from "../../public/images/brands/nordvpn.png";
+import Image17 from "../../public/images/brands/nordvpn.png";
 import Image6 from "../../public/images/brands/obs.png"; //
 import Image10 from "../../public/images/brands/pretty.png";
 import Image5 from "../../public/images/brands/prime.png";
 import Image12 from "../../public/images/brands/romwe.png";
-import Image13 from "../../public/images/brands/rumble.png"; //
+import Image4 from "../../public/images/brands/rumble.png"; //
 import Image14 from "../../public/images/brands/shein.png"; //
-import Image4 from "../../public/images/brands/snapchat.png"; //
+import Image13 from "../../public/images/brands/snapchat.png"; //
 import Image18 from "../../public/images/brands/temu.png"; //
-import Image17 from "../../public/images/brands/tide.png";
-import Image9 from "../../public/images/brands/tiktok.png";
+import Image7 from "../../public/images/brands/tide.png";
+import Image2 from "../../public/images/brands/tiktok.png";
 import Image19 from "../../public/images/brands/wondershare.png"; // 
+import Image20 from "../../public/images/brands/gfuel.png"; //
 
 const Brands = () => {
   return (
@@ -199,6 +200,15 @@ const Brands = () => {
               width={169}
               height={169}
             />
+            <Image
+              loading="eager"
+              quality={100}
+              className="logo-div"
+              src={Image20}
+              alt="brand"
+              width={169}
+              height={169}
+            />
           </div>
           <div className="brand-slide flex-shrink-0 delay flex pl-20 flex-row w-max gap-20">
             <Image
@@ -370,6 +380,15 @@ const Brands = () => {
               quality={100}
               className="logo-div"
               src={Image19}
+              alt="brand"
+              width={169}
+              height={169}
+            />
+            <Image
+              loading="eager"
+              quality={100}
+              className="logo-div"
+              src={Image20}
               alt="brand"
               width={169}
               height={169}
