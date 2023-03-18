f57ef4f74c22dd0d709d8b8c4d62512adff480cb
motionbyfabi <fabiodinota@gmail.com>
2023-03-18 01:48:23 +0100

    try without slug

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,21 +1,7 @@
-import { getStaticPaths } from "../our-talents/[slug]";
-
 export default async function handler(req, res) {  
-    try {
-        // grab static paths using the same method Next.js would use
-        const staticPaths = await getStaticPaths()
-    
-        // get an array of promises
-        const revalidatePaths = staticPaths.map((path) =>
-              res.unstable_revalidate(`/our-talents/${path.params.slug}`)
-        );
-    
-        // run revalidation in parallel
-        await Promise.all([res.unstable_revalidate('/our-talents'), ...revalidatePaths]);
-
+    try {     
         await res.revalidate('/our-talents')
         await res.revalidate('/')
-        
         return res.status(200).send()
       } catch (err) {
         console.error(err)
