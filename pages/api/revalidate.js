5d42a37124cbce2ce098eb705b453c7ca7cb3b4b
motionbyfabi <fabiodinota@gmail.com>
2023-03-18 01:50:28 +0100

    ?

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     try {     
         await res.revalidate('/our-talents')
         await res.revalidate('/')
-        return res.status(200).send()
+        return res.json({ revalidated: true });
       } catch (err) {
         console.error(err)
         return res.status(500).send('Revalidation Error')
