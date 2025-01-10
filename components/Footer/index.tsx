b1a5f94e3d596ebe0c6509d93855c827c78f1329
MatthewBeyondVision <mathew@beyondvisionltd.org>
2025-01-10 17:32:57 +0100

    Footer update

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,33 +1,33 @@
-import Image from "next/image";
-import React from "react";
-import Instagram from "../../public/images/Instagram.png";
-import LinkedIn from "../../public/images/LinkedIn.png";
-import Twitter from "../../public/images/Twitter.png";
+import React from 'react';
+import styles from './Footer.module.css';
 
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
-    <>
-      <div className="flex w-full justify-center items-center flex-col pb-[60px]">
-        <h1 className="text-yellow text-[24px] text-center font-semibold">
-          Looking Beyond The Vision
-        </h1>
-        <div className="flex flex-row justify-center items-center pt-2">
-          <div className="px-2">
-            <a href="https://www.instagram.com/beyondvisionltd/">
-              <Image src={Instagram} alt="social" />
-            </a>
-          </div>
-          <div className="px-2">
-            <a href="https://www.linkedin.com/company/beyond-vision-ltd/">
-              <Image src={LinkedIn} alt="social" />
-            </a>
-          </div>
+    <footer className={styles.footer}>
+      <div className={styles.linksSection}>
+        <div className={styles.column}>
+          <h4>About</h4>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
         </div>
-        <p className="text-yellow opacity-50 pt-1  font-regular text-[16px]">
-          © 2023 by Beyond Vision
-        </p>
+        <div className={styles.column}>
+          <h4>Company</h4>
+          <ul>
+            <li><a href="#">What we do</a></li>
+            <li><a href="#">Case studies</a></li>
+            <li><a href="#">Our talents</a></li>
+          </ul>
+        </div>
       </div>
-    </>
+      <div className={styles.contactSection}>
+        <p><span className={styles.highlight}>Brand? Creator?</span></p>
+        <p><span className={styles.highlight}>Let's Talk.</span></p>
+        <button className={styles.contactButton}>Contact us</button>
+      </div>
+    </footer>
   );
 };
 
