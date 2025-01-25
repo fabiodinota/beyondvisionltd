4fd95f0757ec38ce226d1a4214096d1dc911e17e
MatthewBeyondVision <mathew@beyondvisionltd.org>
2025-01-25 14:52:13 +0100

    next.config.js video update

--- next.config.js
+++ next.config.js
@@ -6,5 +6,20 @@ const nextConfig = {
     domains: ["i.imgur.com", "s3.us-west-2.amazonaws.com"],
   },
 };
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/videos/:path*",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "video/mp4",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
