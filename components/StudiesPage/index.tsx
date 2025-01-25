5266958f6877b1848fe59d943249c7795c3152b4
MatthewBeyondVision <mathew@beyondvisionltd.org>
2025-01-25 15:21:29 +0100

    vimeo mby

--- components/StudiesPage/index.tsx
+++ components/StudiesPage/index.tsx
@@ -7,16 +7,17 @@ const StudiesPage: React.FC = () => {
     AOS.init();
   }, []);
 
-  const videos = [
-    '/videos/01.mp4',
-    '/videos/02.mp4',
-    '/videos/03.mp4',
-    '/videos/04.mp4',
-    '/videos/05.mp4',
-    '/videos/06.mp4',
-    '/videos/07.mp4',
-    '/videos/08.mp4',
-    '/videos/09.mp4',
+  // Představme si, že máme URL pro každý video na Vimeo
+  const vimeoVideos = [
+    'https://player.vimeo.com/video/1050310564?h=ae010c3068',
+    'https://player.vimeo.com/video/1050310506?h=8114864a33',
+    'https://player.vimeo.com/video/1050310395?h=c4c5b71eb3',
+    'https://player.vimeo.com/video/1050310293?h=6a7118a90c',
+    'https://player.vimeo.com/video/1050310256?h=67d9433ee1',
+    'https://player.vimeo.com/video/1050310221?h=adca3b3d4e',
+    'https://player.vimeo.com/video/1050310175?h=3adfed800b',
+    'https://player.vimeo.com/video/1050310080?h=4f06a0c874',
+    'https://player.vimeo.com/video/1050310031?h=72a62fa6e9',
   ];
 
   return (
@@ -31,7 +32,7 @@ const StudiesPage: React.FC = () => {
           Case Studies
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
-          {videos.map((src, index) => (
+          {vimeoVideos.map((videoUrl, index) => (
             <div
               key={index}
               data-aos="slide-up"
@@ -39,18 +40,13 @@ const StudiesPage: React.FC = () => {
               data-aos-delay={`${index * 100}`}
               className="relative flex justify-center"
             >
-              <video 
-                width="324" 
-                height="576" 
-                controls 
-                autoPlay 
-                muted 
-                loop
-                className="rounded-lg shadow-lg"
-              >
-                <source src={src} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
+              <iframe
+                src={`${videoUrl}?title=0&byline=0&portrait=0&badge=0&autopause=0`}
+                frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                title={`video-${index + 1}`}
+              ></iframe>
             </div>
           ))}
         </div>
@@ -60,4 +56,3 @@ const StudiesPage: React.FC = () => {
 };
 
 export default StudiesPage;
-
